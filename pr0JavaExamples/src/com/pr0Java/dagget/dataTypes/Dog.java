c5cedb82810028f453961190ca825ab7ae2f4e58
Roman Kaminskiy - notebook <derrki@gmail.com>
2018-09-04 10:13:05 +0300

    3. Приклад роботи з примітивними та ссилочними типами даних

---
+++ pr0JavaExamples/src/com/pr0Java/dagget/dataTypes/Dog.java
@@ -0,0 +1,18 @@
+package com.pr0Java.dagget.dataTypes;
+
+public class Dog {
+
+    String dogName;
+
+    public Dog(String dogName) {
+        this.dogName = dogName;
+    }
+
+    public String getDogName() {
+        return dogName;
+    }
+
+    public void setDogName(String dogName) {
+        this.dogName = dogName;
+    }
+}
--- pr0JavaExamples/src/com/pr0Java/dagget/dataTypes/Main.java
+++ pr0JavaExamples/src/com/pr0Java/dagget/dataTypes/Main.java
@@ -8,6 +8,36 @@ public class Main {
         print("Hello ");
         println("world !");
         printf("Hello %s", "world !");
+        println("");
+
+        //------------------------------------------
+
+        println("Приклад примітивних і ссилочних типів даних в ava");
+        println("");
+        println("Примітивні типи");
+
+        //змінні примітивних тпів передаються по значенню
+
+        int i = 4;
+        int j = i;
+        println("i = " + i + "   j = " + j);
+        ++j;
+        println("Після інкременту ++j");
+        println("i = " + i + "   j = " + j);
+
+        //змінні ссилочних типів даних передаються по посиланню
+
+        println("");
+        println("силочні типи");
+        Dog thisDog = new Dog("Sparky");
+        Dog thetDog = thisDog;
+        println("thisDog = " + thisDog.getDogName() + "   thatDog = " + thetDog.getDogName());
+        println("");
+        thetDog.setDogName("Wolfie");
+        println("Після присвоєння thetDog Wolfie");
+        println("");
+        println("thisDog = " + thisDog.getDogName() + "   thatDog = " + thetDog.getDogName());
+
 
     }
 }

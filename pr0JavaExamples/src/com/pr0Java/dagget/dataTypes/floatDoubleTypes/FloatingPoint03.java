44f72ece50e73b5394b59c82dfd9db50059a8d29
Roman Kaminskiy - notebook <derrki@gmail.com>
2018-09-06 00:13:55 +0300

    8.2. Робота з типом double (повний приклад + доповнення)

--- pr0JavaExamples/src/com/pr0Java/dagget/dataTypes/floatDoubleTypes/FloatingPoint03.java
+++ pr0JavaExamples/src/com/pr0Java/dagget/dataTypes/floatDoubleTypes/FloatingPoint03.java
@@ -5,5 +5,16 @@ import static util.Print.*;
 public class FloatingPoint03 {
     public static void main(String[] args) {
 
+        double dd = 3.33;
+        float ff = (float) dd;
+        println("dd = " +dd);
+        println("ff = " + ff);
+        println("ff == dd is " + (ff == dd));
+        println("------------");
+        dd = ff;
+        println("dd = " + dd);
+        println("ff = " + ff);
+        println("ff == dd is " + (ff == dd));
+
     }
 }

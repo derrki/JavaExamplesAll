4aa96c35e2a703a5abb09c8cc53898003ae7114d
Roman Kaminskiy - notebook <derrki@gmail.com>
2018-09-06 21:14:31 +0300

    9. Робота з приведенням типів (приклад явного приведення типів)

---
+++ pr0JavaExamples/src/com/pr0Java/dagget/dataTypes/TypeConvercion/TypeConv01.java
@@ -0,0 +1,28 @@
+package com.pr0Java.dagget.dataTypes.TypeConvercion;
+
+import static util.Print.*;
+
+public class TypeConv01 {
+    public static void main(String[] args) {
+
+        //приклад явного приведення типів
+        byte b = 127;
+        short s = b;
+        int i = s;
+        long l = i;
+        float f = l;
+        double d = f;
+        println("b = " + b + " s = " + s + " i = " + i + " l = " + l + " f = " + f + " d = " + d);
+        char c = 0x401;
+        i = c;
+        l = c;
+        f = c;
+        d = c;
+        println("c = " + c + " i = " + i + " l = " + l + " f = " + f + " d = " + d);
+        println("\nПриведення типів з втратою точності");
+        i = 0b11_1111_1111_1111_1111_1111_1111;
+        f = i;
+        println("i = " + i);
+        println("f = " + f);
+    }
+}

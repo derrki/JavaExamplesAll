98800527bdd7bda153aacd3eb97993786424e78e
Roman Kaminskiy - notebook <derrki@gmail.com>
2018-09-08 10:09:20 +0300

    11. Додано нові методи в бібліотеку Print.java

--- pr0JavaExamples/src/util/Print.java
+++ pr0JavaExamples/src/util/Print.java
@@ -14,29 +14,53 @@ public class Print {
         System.out.println(object);
     }
 
-    public static void printlnByte(byte dataByte){
-        String dataString = Integer.toBinaryString(dataByte & 0xFF);
-        System.out.println(dataString);
-    }
-
-    public static void printlnInt (int dataInt) {
-        int groupSize = 4;
-            StringBuilder result = new StringBuilder();
-
-            for (int i = 31; i >= 0; i--) {
-                int mask = 1 << i;
-                result.append((dataInt & mask) != 0 ? "1" : "0");
-
-                if (i % groupSize == 0)
-                    result.append(" ");
-            }
-            result.replace(result.length() - 1, result.length(), "");
-
-        System.out.println(result.toString());
-    }
-
-    public static void printf(String data, Object...args){
+    public static void printf(String data, Object... args) {
         System.out.printf(data, args);
     }
 
+    public static void printByte(byte printByte) {
+        byte byteForPrint = (byte) printByte;
+        String byteBinayStr = String.format("%8s",
+                Integer.toBinaryString(byteForPrint & 0xFF)).replace(' ', '0');
+        byteBinayStr = byteBinayStr.substring(0, 4) + "_"
+                + byteBinayStr.substring(4);
+        print(byteBinayStr);
+    }
+
+    public static void printlnByte(byte printByte) {
+        byte byteForPrint = (byte) printByte;
+        String byteBinayStr = String.format("%8s",
+                Integer.toBinaryString(byteForPrint & 0xFF)).replace(' ', '0');
+        byteBinayStr = byteBinayStr.substring(0, 4) + "_"
+                + byteBinayStr.substring(4);
+        println(byteBinayStr);
+    }
+
+    public static void printInt(int printInt) {
+        String intBinayStr = String.format("%32s",
+                Integer.toBinaryString(printInt)).replace(' ', '0');
+        intBinayStr = intBinayStr.substring(0, 4) + "_"
+                + intBinayStr.substring(4, 8) + "_"
+                + intBinayStr.substring(8, 12) + "_"
+                + intBinayStr.substring(12, 16) + "_"
+                + intBinayStr.substring(16, 20) + "_"
+                + intBinayStr.substring(20, 24) + "_"
+                + intBinayStr.substring(24, 28) + "_"
+                + intBinayStr.substring(28);
+        print(intBinayStr);
+    }
+
+    public static void printlnInt(int printInt) {
+        String intBinayStr = String.format("%32s",
+                Integer.toBinaryString(printInt)).replace(' ', '0');
+        intBinayStr = intBinayStr.substring(0, 4) + "_"
+                + intBinayStr.substring(4, 8) + "_"
+                + intBinayStr.substring(8, 12) + "_"
+                + intBinayStr.substring(12, 16) + "_"
+                + intBinayStr.substring(16, 20) + "_"
+                + intBinayStr.substring(20, 24) + "_"
+                + intBinayStr.substring(24, 28) + "_"
+                + intBinayStr.substring(28);
+        println(intBinayStr);
+    }
 }

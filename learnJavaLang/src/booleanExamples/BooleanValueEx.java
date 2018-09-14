ca99b3badbeb65807dd2177e5737356a64736443
Roman Kaminskiy - notebook <derrki@gmail.com>
2018-09-14 11:24:42 +0300

    21. Приклад роботи з методом booleanValue() класу Boolean.

---
+++ learnJavaLang/src/booleanExamples/BooleanValueEx.java
@@ -0,0 +1,16 @@
+package booleanExamples;
+
+public class BooleanValueEx {
+    public static void main(String[] args) {
+        Boolean mBooleanWrapper;
+        boolean mBooleanPrimitive;
+
+        mBooleanWrapper = new Boolean(true);
+        mBooleanPrimitive = mBooleanWrapper.booleanValue();
+
+        String str = "Primitive value of Boolean object " + mBooleanWrapper + " is " + mBooleanPrimitive;
+        System.out.println(str);
+
+    }
+
+}

d76d4855a054c1d181ec9f27e9beeaa5802696b4
Roman Kaminskiy - notebook <derrki@gmail.com>
2018-09-15 13:36:42 +0300

    22.1 Приклад роботи з методом doubleValue() класу Number. Переіменовано клас, змінено структуру класу.

---
+++ learnJavaLang/src/NumberExamples/NumberClassEx.java
@@ -0,0 +1,20 @@
+package NumberExamples;
+
+public class NumberClassEx {
+    public static void main(String[] args) {
+        Integer x = new Integer(123456);
+        Float y = new Float(9876f);
+
+        // print their value as byte
+        System.out.println("x as integer: " + x
+                + ", x as byte: " + x.byteValue());
+        System.out.println("y as float: " + y
+                + ", y as byte: " + y.byteValue());
+
+        // print their value as double
+        System.out.println("\nx as integer :" + x
+                + ", x as double:" + x.doubleValue());
+        System.out.println("y as float::" + y
+                + ", y as double:" + y.doubleValue());
+    }
+}

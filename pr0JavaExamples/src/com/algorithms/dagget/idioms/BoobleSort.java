f0fc9281314d3fe3d33bae4a5b94697ac50bb5d3
Roman Kaminskiy - notebook <derrki@gmail.com>
2018-09-20 22:13:34 +0300

    27.2 Робота з масивами (алгоритм сортування масиву бульбашкою варіант три))

--- pr0JavaExamples/src/com/algorithms/dagget/idioms/BoobleSort.java
+++ pr0JavaExamples/src/com/algorithms/dagget/idioms/BoobleSort.java
@@ -7,7 +7,7 @@ import static util.Print.*;
 
 public class BoobleSort {
     public static void main(String[] args) {
-        int[] array = new int[6];
+        int[] array = new int[20];
 
         for (int i = 0; i < array.length; i++) {
             array[i] = randomInRange(1, 20);
@@ -20,11 +20,15 @@ public class BoobleSort {
         origynalArray(array);
 
         boobleSortTwo(array);
+
+        origynalArray(array);
+
+        boobleSortThree(array);
     }
 
     private static void origynalArray(int[] array) {
         println("масив до сортування");
-        for (int dataArray: array) {
+        for (int dataArray : array) {
             print(" " + dataArray);
         }
     }
@@ -35,33 +39,58 @@ public class BoobleSort {
         int temp;
         int count = 0;
         for (int i = 0; i < copyArray.length; i++) {
-            for (int j = 0; j < copyArray.length-1; j++) {
-                if (copyArray[j] > copyArray[j+1]){
-                    count ++;
+            for (int j = 0; j < copyArray.length - 1; j++) {
+                if (copyArray[j] > copyArray[j + 1]) {
+                    count++;
                     temp = copyArray[j];
-                    copyArray[j] = copyArray[j+1];
-                    copyArray[j+1] = temp;
+                    copyArray[j] = copyArray[j + 1];
+                    copyArray[j + 1] = temp;
                     println(" " + Arrays.toString(copyArray) + " - i = " + i + "  j = " + j + "  + count = " + count);
                 }
             }
         }
     }
+
     private static void boobleSortTwo(int[] array) {
         println("\nмасив після сортування метод 2");
         int[] copyArray = Arrays.copyOf(array, array.length);
         int temp;
         int count = 0;
         for (int i = 0; i < copyArray.length; i++) {
-            for (int j = 0; j < copyArray.length-1-i; j++) {
-                if (copyArray[j] > copyArray[j+1]){
-                    count ++;
+            for (int j = 0; j < copyArray.length - 1 - i; j++) {
+                if (copyArray[j] > copyArray[j + 1]) {
+                    count++;
                     temp = array[j];
-                    copyArray[j] = copyArray[j+1];
-                    copyArray[j+1] = temp;
+                    copyArray[j] = copyArray[j + 1];
+                    copyArray[j + 1] = temp;
                     println(" " + Arrays.toString(copyArray) + " - i = " + i + "  j = " + j + "  + count = " + count);
                 }
             }
         }
+    }
+
+    private static void boobleSortThree(int[] array) {
+        println("\nмасив після сортування метод 3");
+        int[] copyArray = Arrays.copyOf(array, array.length);
+        int temp;
+        int count = 0;
+        for (int i = copyArray.length - 1; i >= 2; i--) {
+            boolean sorted = true;
+            for (int j = 0; j < i; j++) {
+                if (copyArray[j] > copyArray[j + 1]) {
+                    ++count;
+                    temp = array[j];
+                    copyArray[j] = copyArray[j + 1];
+                    copyArray[j + 1] = temp;
+                    println(" " + Arrays.toString(copyArray) + " - i = " + i + "  j = " + j + "  + count = " + count);
+                    sorted = false;
+                }
+            }
+            if (sorted) {
+                break;
+            }
+        }
+
     }
 }
 

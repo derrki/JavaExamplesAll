495cc0cdc39c86cbf0ef5083dde215c1e5d09143
Roman Kaminskiy - notebook <derrki@gmail.com>
2018-09-20 15:10:30 +0300

    26.1 Робота з методами класу BigDecimal (приклади округлення)

--- pr0JavaExamples/src/com/pr0Java/dagget/dataTypes/BigIntegetAndBidDecimal/BigDecimalClass.java
+++ pr0JavaExamples/src/com/pr0Java/dagget/dataTypes/BigIntegetAndBidDecimal/BigDecimalClass.java
@@ -29,5 +29,99 @@ public class BigDecimalClass {
         println("bd3 = " + bd3);
         bd3 = BigDecimal.valueOf(0.3);
         println("bd3 = " + bd3);
+
+        //приклади заокруглення
+        bd3 = new BigDecimal(0.3);
+        //bd3.setScale(1); //уде помилка
+        println("ROUND_CEILING bd3 = " + bd3.setScale(1, BigDecimal.ROUND_CEILING));
+
+        // ROUND_CEILING: В більшу сторону
+        println();
+        println("ROUND_CEILING: В більшу сторону");
+        BigDecimal bdr = new BigDecimal("0.333");
+        print("bdr =  " + bdr);
+        println("   ROUND_CEILING bdr =  " + bdr.setScale(2, BigDecimal.ROUND_CEILING));
+        bdr = new BigDecimal("-0.333");
+        print("bdr = " + bdr);
+        println("   ROUND_CEILING bdr = " + bdr.setScale(2, BigDecimal.ROUND_CEILING));
+
+        // ROUND_DOWN: В меншу сторону по модулю
+        println();
+        println("ROUND_DOWN: Заокруглення в меншу сторону по модулю");
+        bdr = new BigDecimal("0.333");
+        print("bdr =  " + bdr);
+        println("   ROUND_DOWN bdr =  " + bdr.setScale(2, BigDecimal.ROUND_DOWN));
+        bdr = new BigDecimal("-0.333");
+        print("bdr = " + bdr);
+        println("   ROUND_DOWN bdr = " + bdr.setScale(2, BigDecimal.ROUND_DOWN));
+
+        // ROUND_UP: В більшу сторону по модулю
+        println();
+        println("ROUND_UP: В більшу сторону по модулю");
+        bdr = new BigDecimal("0.333");
+        print("bdr =  " + bdr);
+        println("   ROUND_UP bdr =  " + bdr.setScale(2, BigDecimal.ROUND_UP));
+        bdr = new BigDecimal("-0.333");
+        print("bdr = " + bdr);
+        println("   ROUND_UP bdr = " + bdr.setScale(2, BigDecimal.ROUND_UP));
+
+        // ROUND_FLOOR: В меншу сторону
+        println();
+        println("ROUND_FLOOR: В меншу сторону");
+        bdr = new BigDecimal("0.333");
+        print("bdr =  " + bdr);
+        println("   ROUND_FLOOR bdr =  " + bdr.setScale(2, BigDecimal.ROUND_FLOOR));
+        bdr = new BigDecimal("-0.333");
+        print("bdr = " + bdr);
+        println("   ROUND_FLOOR bdr = " + bdr.setScale(2, BigDecimal.ROUND_FLOOR));
+
+        // ROUND_HALF_UP: Заокруглення вгору, якщо число после коми >= .5
+        println();
+        println("ROUND_HALF_UP: Заокруглення вгору, якщо число после коми >= .5");
+        bdr = new BigDecimal("0.335");
+        print("bdr =  " + bdr);
+        println("   ROUND_HALF_UP bdr =  " + bdr.setScale(2, BigDecimal.ROUND_HALF_UP));
+        bdr = new BigDecimal("-0.335");
+        print("bdr = " + bdr);
+        println("   ROUND_HALF_UP bdr = " + bdr.setScale(2, BigDecimal.ROUND_HALF_UP));
+        bdr = new BigDecimal("0.333");
+        print("bdr =  " + bdr);
+        println("   ROUND_HALF_UP bdr =  " + bdr.setScale(2, BigDecimal.ROUND_HALF_UP));
+        bdr = new BigDecimal("-0.333");
+        print("bdr = " + bdr);
+        println("   ROUND_HALF_UP bdr = " + bdr.setScale(2, BigDecimal.ROUND_HALF_UP));
+
+        // ROUND_HALF_DOWN: Заокруглення вгору, якщо число после коми > .5
+        println();
+        println("ROUND_HALF_DOWN: Заокруглення вгору, якщо число после коми > .5");
+        bdr = new BigDecimal("0.335");
+        print("bdr =  " + bdr);
+        println("   ROUND_HALF_DOWN bdr =  " + bdr.setScale(2, BigDecimal.ROUND_HALF_DOWN));
+        bdr = new BigDecimal("-0.335");
+        print("bdr = " + bdr);
+        println("   ROUND_HALF_DOWN bdr = " + bdr.setScale(2, BigDecimal.ROUND_HALF_DOWN));
+        bdr = new BigDecimal("0.336");
+        print("bdr =  " + bdr);
+        println("   ROUND_HALF_DOWN bdr =  " + bdr.setScale(2, BigDecimal.ROUND_HALF_DOWN));
+        bdr = new BigDecimal("-0.336");
+        print("bdr = " + bdr);
+        println("   ROUND_HALF_DOWN bdr = " + bdr.setScale(2, BigDecimal.ROUND_HALF_DOWN));
+
+        // ROUND_HALF_EVEN: Заокруглення по парності
+        println();
+        println("ROUND_HALF_EVEN: Заокруглення по парності");
+        bdr = new BigDecimal("0.335");
+        print("bdr =  " + bdr);
+        println("  ROUND_HALF_EVEN bdr =  " + bdr.setScale(2, BigDecimal.ROUND_HALF_EVEN));
+        bdr = new BigDecimal("0.325");
+        print("bdr =  " + bdr);
+        println("  ROUND_HALF_EVEN bdr =  " + bdr.setScale(2, BigDecimal.ROUND_HALF_EVEN));
+
+        bdr = new BigDecimal("-0.335");
+        print("bdr = " + bdr);
+        println("  ROUND_HALF_EVEN bdr = " + bdr.setScale(2, BigDecimal.ROUND_HALF_EVEN));
+        bdr = new BigDecimal("-0.325");
+        print("bdr = " + bdr);
+        println("  ROUND_HALF_EVEN bdr = " + bdr.setScale(2, BigDecimal.ROUND_HALF_EVEN));
     }
 }

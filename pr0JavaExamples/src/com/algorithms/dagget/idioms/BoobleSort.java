c0950a7a23dac6f2f6e362532266c9fbe917aedc
Roman Kaminskiy - notebook <derrki@gmail.com>
2018-09-20 21:56:01 +0300

    27.1 Робота з масивами (алгоритм сортування масиву бульбашкою варіант два (швидший))

--- pr0JavaExamples/src/com/algorithms/dagget/idioms/BoobleSort.java
+++ pr0JavaExamples/src/com/algorithms/dagget/idioms/BoobleSort.java
@@ -7,26 +7,58 @@ import static util.Print.*;
 
 public class BoobleSort {
     public static void main(String[] args) {
-        int[] array = new int[5];
+        int[] array = new int[6];
 
         for (int i = 0; i < array.length; i++) {
             array[i] = randomInRange(1, 20);
         }
 
+        origynalArray(array);
+
+        boobleSortOne(array);
+
+        origynalArray(array);
+
+        boobleSortTwo(array);
+    }
+
+    private static void origynalArray(int[] array) {
         println("масив до сортування");
         for (int dataArray: array) {
             print(" " + dataArray);
         }
+    }
 
-        println("\nмасив після сортування");
+    private static void boobleSortOne(int[] array) {
+        println("\nмасив після сортування метод 1");
+        int[] copyArray = Arrays.copyOf(array, array.length);
         int temp;
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array.length-1; j++) {
-                if (array[j] > array[j+1]){
+        int count = 0;
+        for (int i = 0; i < copyArray.length; i++) {
+            for (int j = 0; j < copyArray.length-1; j++) {
+                if (copyArray[j] > copyArray[j+1]){
+                    count ++;
+                    temp = copyArray[j];
+                    copyArray[j] = copyArray[j+1];
+                    copyArray[j+1] = temp;
+                    println(" " + Arrays.toString(copyArray) + " - i = " + i + "  j = " + j + "  + count = " + count);
+                }
+            }
+        }
+    }
+    private static void boobleSortTwo(int[] array) {
+        println("\nмасив після сортування метод 2");
+        int[] copyArray = Arrays.copyOf(array, array.length);
+        int temp;
+        int count = 0;
+        for (int i = 0; i < copyArray.length; i++) {
+            for (int j = 0; j < copyArray.length-1-i; j++) {
+                if (copyArray[j] > copyArray[j+1]){
+                    count ++;
                     temp = array[j];
-                    array[j] = array[j+1];
-                    array[j+1] = temp;
-                    println(" " + Arrays.toString(array));
+                    copyArray[j] = copyArray[j+1];
+                    copyArray[j+1] = temp;
+                    println(" " + Arrays.toString(copyArray) + " - i = " + i + "  j = " + j + "  + count = " + count);
                 }
             }
         }

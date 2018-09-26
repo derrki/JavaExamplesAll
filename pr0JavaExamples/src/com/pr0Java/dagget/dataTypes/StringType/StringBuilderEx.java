0dcd66ed274c6492dd0fa12778ce101da2c8ad07
Roman Kaminskiy - notebook <derrki@gmail.com>
2018-09-26 10:16:22 +0300

    31 Робота з StringBuilder

---
+++ pr0JavaExamples/src/com/pr0Java/dagget/dataTypes/StringType/StringBuilderEx.java
@@ -0,0 +1,11 @@
+package com.pr0Java.dagget.dataTypes.StringType;
+
+import static util.Print.*;
+
+public class StringBuilderEx {
+    public static void main(String[] args) {
+        StringBuilder stringBuilderOne = new StringBuilder();
+        println("stringBuilderOne.length() = " + stringBuilderOne.length());
+        println("stringBuilderOne.capacity() = " + stringBuilderOne.capacity());
+    }
+}
